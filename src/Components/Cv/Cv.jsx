2db35ae4f188d0ead98381e6fb60c9348ba1f1f0
nacim <nacimmakedhi@gmail.com>
2025-09-29 01:18:35 +0200

    cv component updated

--- src/Components/Cv/Cv.jsx
+++ src/Components/Cv/Cv.jsx
@@ -3,11 +3,11 @@ import useLanguage from '../../Context/LanguageContext'
 
 export default function Cv({cv}){
 
-    const {language} = useLanguage();
+    const {data , language} = useLanguage();
 
     return (
         <div className={styles.cv_container}>
-            <a href={cv.link} target="_blank" rel="noopener noreferrer">
+            <a href={data[0].cv_link} target="_blank" rel="noopener noreferrer">
                 <div className={styles.left_section}>
                     <div className={styles.left_section_icon}>
                         <div className={styles.file_icon}>
@@ -21,7 +21,7 @@ export default function Cv({cv}){
                         <div className={styles.cv_details}>
                             <p>{cv.type}</p>
                             <span></span>
-                            <p>{cv.updatedOn.toLocaleString(language === "en" ? "en-EN" : "fr-FR", { month: "long" })} ${cv.updatedOn.getFullYear()}</p>
+                            <p>{cv.updatedOn.toLocaleString(language === "en" ? "en-EN" : "fr-FR", { month: "long" })} {cv.updatedOn.getFullYear()}</p>
                             <span></span>
                             <p>{`${cv.size} MB`}</p>
                         </div>
@@ -29,7 +29,7 @@ export default function Cv({cv}){
                 </div>
 
                 <div className={styles.right_section}>
-                        <a href={cv.link} target="_blank" rel="noopener noreferrer">
+                        <a href={data[0].cv_link} target="_blank" rel="noopener noreferrer">
                             <span className="material-symbols-outlined" style={{color : "white"}}>download</span>
                         </a>
                 </div>
